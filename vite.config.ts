35c0dafadfa12addde1db661fcbc20f8e5c1126e
IESLaFresneda <ProyectosIES_La_Fresneda@outlook.es>
2026-05-24 20:15:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
-  base: '/Pantallas-digitales-Primer-Ciclo-ESO/',
+  base: './',
 
   plugins: [react(), tailwindcss()],
+});
+
 
   resolve: {
     alias: {
